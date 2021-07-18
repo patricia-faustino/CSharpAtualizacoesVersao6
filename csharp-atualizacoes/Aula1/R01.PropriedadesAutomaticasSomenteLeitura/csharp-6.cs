2963f3d6ccae87cfce9317f3b31a22d996621708
Patricia Faustino de Brito de Jesus <patricia.faustino@iteris.com.br>
2021-07-18 14:16:42 -0300

    Iniciando o projeto e implementando propriedades somente leitura automaticas

---
+++ csharp-atualizacoes/Aula1/R01.PropriedadesAutomaticasSomenteLeitura/csharp-6.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp6.R01
+{
+    class Programa
+    {
+        public void Main()
+        {
+            Console.WriteLine("1. Propriedades Automáticas Somente-Leitura");
+
+            Aluno aluno = new Aluno("Marty", "McFly", new DateTime(1968, 6, 12));
+
+            Console.WriteLine(aluno.Nome);
+            Console.WriteLine(aluno.Sobrenome);
+            Console.WriteLine(aluno);
+
+        }
+    }
+
+    class Aluno
+    {
+
+        public string Nome { get; }
+        public string Sobrenome { get; }
+        public DateTime DataNascimento { get; }
+
+        public Aluno(string nome, string sobrenome, DateTime dataNascimento)
+        {
+            Nome = nome;
+            Sobrenome = sobrenome;
+            DataNascimento = dataNascimento;
+        }
+        public override string ToString()
+        {
+            return $"{Nome} {Sobrenome}"; 
+        }
+    }
+
+}
+
---
+++ csharp-atualizacoes/Aula1/R02.InicializadoresDePropriedadeAutomatica/csharp-6.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp6.R02
+{
+    class Programa
+    {
+        public void Main()
+        {
+            Console.WriteLine("2. Inicializadores De Propriedade Automática");
+
+
+        }
+    }
+
+   
+}
